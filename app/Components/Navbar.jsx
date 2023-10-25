9b68c051f9dd51aab2371fc8fe72bcc67a6f980c
adityap5 <aditya29pippal@gmail.com>
2023-10-25 22:10:31 +0530

    first

---
+++ app/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+ <>
+  <div className="nav">
+    <img src="nike-react\app\images\brand_logo.png" alt="" />
+    </div> 
+ </>
+  )
+}
+
+export default Navbar
